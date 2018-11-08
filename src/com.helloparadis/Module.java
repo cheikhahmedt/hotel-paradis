159a590fba27bdea1fef45d404ead4304ad791c3
ndoyepato <issandmb19@gmail.com>
2018-11-08 21:17:50 +0000

    Classe Etudiant

---
+++ src/com.helloparadis/Module.java
@@ -0,0 +1,23 @@
+class Module{
+    private String nom;
+    private int coef;
+
+    public Module() {
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public int getCoef() {
+        return coef;
+    }
+
+    public void setCoef(int coef) {
+        this.coef = coef;
+    }
+}
