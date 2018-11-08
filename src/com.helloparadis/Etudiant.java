aad8fb4149c6c85a53a42c08904f6c947e384792
ndoyepato <issandmb19@gmail.com>
2018-11-08 21:12:23 +0000

    Classe Etudiant

---
+++ src/com.helloparadis/Etudiant.java
@@ -0,0 +1,23 @@
+class Etudiant{
+    private String prenom;
+    private String nom;
+
+    public Etudiant() {
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+}
